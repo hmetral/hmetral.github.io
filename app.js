4b2cb9afd410660aaa468c1e48e0f29d824f0e41
Henry Metral <67717950+hmetral@users.noreply.github.com>
2024-02-21 13:11:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const menuBTN = document.querySelector("#menuBTN");
-const openID= document.querySelector("#menuBTN");
-const closeID = document.querySelector("#menuBTN");
-const mobileNav = document.querySelector("#menuBTN");
+const openID= document.querySelector("#openID");
+const closeID = document.querySelector("#closeID");
+const mobilenav = document.querySelector("#mobilenav");
+const mobileLinks = mobilenav.querySelectorAll("a");
 
 function togglemenu() {
     const isExpanded = menuBTN.getAttribute("aria-expanded") === "true";
    
     menuBTN.setAttribute("aria-expanded", !isExpanded);
 
-    mobileNav.setAttribute("aria-hidden", isExpanded);
+    mobilenav.setAttribute("aria-hidden", isExpanded);
 
     if (!isExpanded){
         closeID.style.display = "block";
         openID.style.display = "none";
-        mobileNav.classList.add("open");
+        mobilenav.classList.add("open");
     } else{
         closeID.style.display = "none";
         openID.style.display = "block";
-        mobileNav.classList.remove("open");
+        mobilenav.classList.remove("open");
     }
 }
 
 menuBTN.addEventListener ("click", () => {
     togglemenu();
+});
+
+mobileLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        if(mobilenav.classList.contains("open")){
+            togglemenu();
+        }
+    });
 });
